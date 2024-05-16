d621f73747048fbf046b8a4834e70424a4aceb86
Seongju <lsj40413@gmail.com>
2024-05-16 20:39:28 +0900

    init commit

---
+++ src/test/java/awsspringmessaginglab/sj/SjApplicationTests.java
@@ -0,0 +1,13 @@
+package awsspringmessaginglab.sj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
