4deb0bfbb9bf205dfa75cc525a180ca22d332d60
Seongju <lsj40413@gmail.com>
2024-11-09 10:28:27 +0900

    feat: setting test

--- spring-batch/src/test/java/sj/springBatch/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package sj.springBatch;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
---
+++ spring-batch/src/test/java/sj/springBatch/support/TestTemplate.java
@@ -0,0 +1,43 @@
+package sj.springBatch.support;
+
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.Container.ExecResult;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+@ActiveProfiles("test")
+public class TestTemplate {
+
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "1234";
+    private static final String DB_NAME = "test-spring-batch";
+    private static final int DB_PORT = 3306;
+
+    static final MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:8.0")
+        .withUsername(DB_USERNAME)
+        .withPassword(DB_PASSWORD)
+        .withDatabaseName(DB_NAME)
+        .withExposedPorts(DB_PORT);
+
+    static {
+        Startables.deepStart(mySQLContainer)
+            .join();
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        setProperties();
+    }
+
+    //region Private Methods
+    private static void setProperties() {
+        System.setProperty("spring.datasource.url", mySQLContainer.getJdbcUrl());
+        System.setProperty("spring.datasource.username", DB_USERNAME);
+        System.setProperty("spring.datasource.password", DB_PASSWORD);
+        System.setProperty("spring.jpa.hibernate.ddl-auto", "update");
+    }
+
+}
