1c75b4a1604998fb824ca5a3038c6f154359f90c
Seongju <67941526+Seongju-Lee@users.noreply.github.com>
2024-12-05 00:15:55 +0900

    feat: legacy 프로젝트 파일 제거 (#3)

--- batch2/src/test/java/batch2/support/config/GlobalConfig.java
+++ batch2/src/test/java/batch2/support/config/GlobalConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"sj.springBatch.global", "sj.springBatch.batch2"})
+@ComponentScan(basePackages = {"sj.batch.global", "batch2"})
 public class GlobalConfig {
 }
