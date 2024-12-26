2f20574459a1e73c8d3b4e8ea440a0242b7ba148
Seongju <lsj40413@gmail.com>
2024-12-26 14:37:48 +0900

    feat: batch4 setup

---
+++ batch4/README.md
@@ -0,0 +1,1 @@
+# 4. Step의 기본 구성 그리고, 생성원리와 동작원리 - StepBuilder & Step & Tasklet
---
+++ batch4/src/test/java/batch3/support/config/GlobalConfig.java
@@ -0,0 +1,9 @@
+package batch3.support.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"sj.batch.global", "batch3"})
+public class GlobalConfig {
+}
