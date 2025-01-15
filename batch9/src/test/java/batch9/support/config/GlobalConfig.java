5849c49fc5c9b9389bb842f1c99034ef0f73dddf
Seongju <lsj40413@gmail.com>
2025-01-15 20:17:45 +0900

    feat: batch9 module setup

---
+++ batch9/src/test/java/batch9/support/config/GlobalConfig.java
@@ -0,0 +1,9 @@
+package batch9.support.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"sj.batch.global", "batch8"})
+public class GlobalConfig {
+}
